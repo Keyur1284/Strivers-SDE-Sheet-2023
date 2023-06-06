1d27a6944894df8bb2c5bc3a53fde585eed269f4
Govrani Keyur <202101498@daiict.ac.in>
2023-06-06 17:39:13 +0530

    Solved in best optimized way

--- Day 2/88. Merge Sorted Array.cpp
+++ Day 2/88. Merge Sorted Array.cpp
@@ -32,6 +32,23 @@ public:
 
 
 // Solved without using extra space
+
+// Time Complexity :- O((m + n)* log(m + n))
+// Space Complexity :- O(1)
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        for (int i = m, j = 0; j < n; i++, j++)
+            nums1[i] = nums2[j];
+
+        sort (nums1.begin(), nums1.end());
+    }
+};
+
+
+
 // Time Complexity :- O(n + m)
 // Space Complexity :- O(1)
 
