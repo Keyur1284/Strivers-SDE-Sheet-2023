fcaa38a43e4149f45d45a52113112c1397df3ac1
Govrani Keyur <202101498@daiict.ac.in>
2023-06-08 20:00:43 +0530

    Solved by unordered_map

--- Day 4/Longest Subarray With Sum K (Positive and Negative Values).cpp
+++ Day 4/Longest Subarray With Sum K (Positive and Negative Values).cpp
@@ -29,3 +29,29 @@ int getLongestSubarray(vector<int>& nums, int k){
 
     return maxi;
 }
+
+
+
+#include <bits/stdc++.h> 
+int getLongestSubarray(vector<int>& nums, int k){
+    
+    unordered_map<int, int> mp;
+    int sum = 0, maxi = 0;
+    mp[0] = -1;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+
+        if (mp.find(sum - k) != mp.end())
+        {
+            int len = i - mp[sum - k];
+            maxi = max(maxi, len);
+        }
+
+        if(mp.find(sum) == mp.end())
+            mp[sum] = i;
+    }
+
+    return maxi;
+}
