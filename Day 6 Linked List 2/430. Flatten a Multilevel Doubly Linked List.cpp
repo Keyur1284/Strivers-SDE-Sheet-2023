847c59085fc0d8fc51139c7f31035a880c9c442d
Govrani Keyur <202101498@daiict.ac.in>
2023-06-11 00:05:07 +0530

    Solved by Recursive Approach

---
+++ Day 6 Linked List 2/430. Flatten a Multilevel Doubly Linked List.cpp
@@ -0,0 +1,40 @@
+// Problem Link :- https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+
+    Node* DFS (Node* node, Node* rest)
+    {
+        if (node == NULL) 
+            return rest;
+
+        Node* nextNode = DFS(node->next, rest);
+        node->next = DFS(node->child, nextNode);
+
+        if (node->next) 
+            node->next->prev = node;
+        
+        node->child = nullptr;
+        return node;
+    }
+
+    Node* flatten(Node* head) {
+        
+        Node* ans = DFS (head, NULL);
+        return ans;
+    }
+};
