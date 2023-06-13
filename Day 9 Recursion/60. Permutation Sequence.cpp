ef1d5bbb2072fa5c89a087048fc4e37b38fa59d0
Govrani Keyur <202101498@daiict.ac.in>
2023-06-13 10:34:50 +0530

    Solved in most optimized way

---
+++ Day 9 Recursion/60. Permutation Sequence.cpp
@@ -0,0 +1,73 @@
+// Problem Link :- https://leetcode.com/problems/permutation-sequence/
+
+// Solved by inbuilt next_permutation function
+// Time Complexity :- O(n * n!)
+// Space Complexity :- O(n)
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        
+        string s = "";
+
+        for (int c = 1; c <= n; c++)
+            s += to_string(c);
+
+        do
+        {
+            k--;
+
+        } while (k && next_permutation(s.begin(), s.end()));
+
+        return s;
+    }
+};
+
+
+
+// Solved by Optimized Approach
+
+
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+        vector<int> fact(n);
+        fact[0] = 1;
+
+        for (int i = 1; i < n; i++)
+            fact[i] = i * fact[i - 1];
+
+        string ans = "";
+        k--;
+        vector<bool> vis (n + 1, false);
+
+        for (int i = 1; i <= n; i++)
+        {
+            int rem = n - i;
+            int next = k / fact[rem];
+
+            int val = 1;
+
+            while (true)
+            {
+                if (!vis[val])
+                {
+                    if (next == 0)
+                        break;
+                    
+                    next--;
+                }
+
+                val++;
+            }
+
+            vis[val] = true;
+            ans += val + '0';
+            k %= fact[rem];
+        }
+
+        return ans;
+    }
+};
