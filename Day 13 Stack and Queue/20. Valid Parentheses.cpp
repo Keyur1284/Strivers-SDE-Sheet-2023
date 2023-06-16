3d594a5b81d7e76e90ff098340fd725480714546
Govrani Keyur <202101498@daiict.ac.in>
2023-06-16 17:34:58 +0530

    Solved using stack

---
+++ Day 13 Stack and Queue/20. Valid Parentheses.cpp
@@ -0,0 +1,36 @@
+// Problem Link :- https://leetcode.com/problems/valid-parentheses/
+
+// Solved using stack
+// Time Complexity :- O(n)
+// Space Complexity :- O(n)
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        unordered_map<char, char> mp;
+        mp[')'] = '(';
+        mp['}'] = '{';
+        mp[']'] = '[';
+        
+        for (auto &it : s)
+        {
+            if (it == '(' || it == '{' || it == '[')
+                st.emplace(it);
+
+            else if (st.empty())
+                return false;
+
+            else
+            {
+                if (st.top() != mp[it])
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return (st.empty());
+    }
+};
