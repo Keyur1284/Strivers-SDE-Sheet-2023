5f07fedb62d129f58d6e80b6c35fec79b4dfb566
Govrani Keyur <202101498@daiict.ac.in>
2023-06-16 18:50:06 +0530

    Solved using stack

---
+++ Day 13 Stack and Queue/503. Next Greater Element II.cpp
@@ -0,0 +1,29 @@
+// Problem Link :- https://leetcode.com/problems/next-greater-element-ii/
+
+// Solved using stack
+// Time Complexity :- O(n)  
+// Space Complexity :- O(n)
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector <int> ans(n, -1);
+        stack <int> st;
+        
+        for (int i = 2 * n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && st.top() <= nums[i % n])
+                st.pop();
+            
+            if (!st.empty() && i < n)
+                ans[i] = st.top();
+            
+            st.emplace(nums[i % n]);    
+        }
+        
+        return ans;
+        
+    }
+};
