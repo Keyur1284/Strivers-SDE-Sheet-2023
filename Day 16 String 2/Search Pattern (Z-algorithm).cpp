e2b37c1312d991f613ef9a60c448239ee99724a1
Govrani Keyur <202101498@daiict.ac.in>
2023-06-20 18:52:24 +0530

    Solved by KMP and Z Algorithm

---
+++ Day 16 String 2/Search Pattern (Z-algorithm).cpp
@@ -0,0 +1,111 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/8dcd25918295847b4ced54055eae35a8501181c1/1
+
+// Solved by KMP Algorithm
+// Time Complexity :- O(n + m)
+// Space Complexity :- O(m)
+
+class Solution
+{
+public:
+	vector <int> search(string pat, string txt)
+	{
+		int n = txt.size(), m = pat.size();
+		
+		vector<int> lps(m, 0);
+		int len = 0;
+		
+		for (int i = 1; i < m; )
+		{
+			if (pat[i] == pat[len])
+			{
+				len++;
+				lps[i] = len;
+				i++;
+			}
+			
+			else
+			{
+				if (len == 0)
+					i++;
+					
+				else
+					len = lps[len - 1];
+			}
+		}
+		
+		vector<int> ans;
+		
+		for (int i = 0, j = 0; i < n; )
+		{
+			if (txt[i] == pat[j])
+			{
+				i++;
+				j++;
+				
+				if (j == m)
+				{
+					ans.emplace_back(i - j + 1);
+					j = lps[j - 1];
+				}
+			}
+			
+			else
+			{
+				if (j == 0)
+					i++;
+					
+				else
+					j = lps[j - 1];
+			}
+		}
+		
+		return ans;
+	}
+};
+
+
+
+// Solved by Z Algorithm
+// Time Complexity :- O(n + m)
+// Space Complexity :- O(n + m)
+
+class Solution
+{
+public:
+	vector <int> search(string pat, string txt)
+	{
+		int patLen = pat.length();
+		string s = pat + "$" + txt;
+		int n = s.size();
+		
+		vector<int> z(n, 0);
+		
+		int left = 0, right = 0;
+		
+		for (int i = 1; i < n; i++)
+		{
+			if (right - i > 0)
+				z[i] = min(right - i, z[i - left]);
+				
+			while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+				z[i]++;
+				
+			if (i + z[i] > right)
+			{
+				left = i;
+				right = i + z[i];
+			}
+		}
+		
+		vector<int> ans;
+		
+		for (int i = 1; i < n; i++)
+		{
+			if (z[i] == patLen)
+				ans.emplace_back(i - patLen);
+		}
+		
+		return ans;
+	}
+	
+};
