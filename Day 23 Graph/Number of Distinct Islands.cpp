d3fdbad3115aa9b254a9422d2354fa3103832563
Govrani Keyur <202101498@daiict.ac.in>
2023-07-03 21:21:59 +0530

    Solved by DFS

---
+++ Day 23 Graph/Number of Distinct Islands.cpp
@@ -0,0 +1,52 @@
+//Problem Link :- https://practice.geeksforgeeks.org/problems/number-of-distinct-islands/1
+
+// Solved by DFS
+// Time Complexity :- O(n * m * log(n * m))
+// Space Complexity :- O(n * m)
+
+class Solution {
+  public:
+  
+    int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+    
+    void DFS (int x, int y, int basex, int basey, int n, int m, vector<vector<bool>> &vis, 
+            vector<vector<int>> &vec, vector<vector<int>>& grid)
+    {
+        vis[x][y] = true;
+        vec.push_back({x - basex, y - basey});
+        
+        for (int i = 0; i < 4; i++)
+        {
+            int newx = x + dx[i];
+            int newy = y + dy[i];
+            
+            if (newx >= 0 && newx < n && newy >= 0 && newy < m && grid[newx][newy] && !vis[newx][newy])
+                DFS (newx, newy, basex, basey, n, m, vis, vec, grid);
+        }
+    }
+  
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<bool>> vis (n, vector<bool> (m, false));
+        set<vector<vector<int>>> st;
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] && !vis[i][j])
+                {
+                    vector<vector<int>> vec;
+                    
+                    DFS (i, j, i, j, n, m, vis, vec, grid);
+                    st.emplace(vec); 
+                }
+            }
+        }
+        
+        return (int)st.size();
+    }
+};
