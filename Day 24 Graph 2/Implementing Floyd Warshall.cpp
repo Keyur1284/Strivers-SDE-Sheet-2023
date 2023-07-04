c57393a757aea87e4569a072520fbffcaea1cba5
Govrani Keyur <202101498@daiict.ac.in>
2023-07-04 22:53:39 +0530

    implemented Floyd Warshall Algorithm

---
+++ Day 24 Graph 2/Implementing Floyd Warshall.cpp
@@ -0,0 +1,64 @@
+// Problem Link :- https://practice.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1
+
+// Multi-Source shortest path algorithm
+// Helps to detect negative cycle
+
+// Time Complexity :- O(V^3)
+// Space Complexity :- O(V^2)
+
+//User function template for C++
+
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+
+	    int n = matrix.size(), inf = INT_MAX;
+
+	    for (int i = 0; i < n; i++)
+	    {
+	        for (int j = 0; j < n; j++)
+	        {
+	            if (matrix[i][j] == -1)
+	                matrix[i][j] = inf;
+	        }
+	    }
+
+	    for (int via = 0; via < n; via++)
+	    {
+	        for (int i = 0; i < n; i++)
+    	    {
+    	        for (int j = 0; j < n; j++)
+    	        {
+					if (matrix[i][via] < inf && matrix[via][j] < inf && matrix[i][via] + matrix[via][j] < inf)
+    	            	matrix[i][j] = min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+    	        }
+    	    }
+	    }
+
+        // For detection of negative cycle, we have to write following code
+
+        /*
+            bool checkNegativeCycle (vector<vector<int>>&matrix)
+            {
+                int n = matrix.size();
+
+                for (int node = 0; node < n; node++)
+                {
+                    if (matrix[node][node] < 0)
+                        return true;
+                }
+
+                return false
+            }
+        */
+
+	    for (int i = 0; i < n; i++)
+	    {
+	        for (int j = 0; j < n; j++)
+	        {
+	            if (matrix[i][j] == inf)
+	                matrix[i][j] = -1;
+	        }
+	    }
+	}
+};
