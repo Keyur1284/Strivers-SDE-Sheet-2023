b2e7e82b9db8c5bcbde55abe38526158f92f140f
Govrani Keyur <202101498@daiict.ac.in>
2023-07-05 18:38:54 +0530

    Solved by DP

---
+++ Day 25 Dynamic Programming/0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,180 @@
+//Problem Link :- https://www.codingninjas.com/codestudio/problems/0-1-knapsack_920542
+
+// Solved by Memoization Method
+// Time Complexity :- O(n * maxWeight)
+// Space Complexity :- O(n * maxWeight)
+
+int func (int index, int maxWt, vector<int> &weight, vector<int> &value, vector <vector <int>> &dp)
+{
+    if (index == 0)
+    {
+        if (weight[index] <= maxWt)
+            return value[index];
+        
+        else
+            return 0;
+    }
+    
+    if (dp[index][maxWt] != 0)
+        return dp[index][maxWt];
+    
+    int notTake = 0 + func (index - 1, maxWt, weight, value, dp);
+    int take = INT_MIN;
+    
+    if (weight[index] <= maxWt)
+        take = value[index] + func (index - 1, maxWt - weight[index], weight, value, dp);
+        
+        return dp[index][maxWt] = max (take, notTake);
+}
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+    vector <vector <int>> dp (n, vector <int> (maxWeight + 1, 0));
+    
+    return func (n - 1, maxWeight, weight, value, dp);
+}
+
+
+
+//Solved by Tabulation Method
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWt) 
+{
+    vector <vector <int>> dp (n, vector <int> (maxWt + 1, 0));
+    
+    for (int i = weight[0]; i <= maxWt; i++)
+        dp[0][i] = value[0];
+    
+    for (int index = 1; index < n; index++)
+    {
+        for (int cap = 0; cap <= maxWt; cap++)
+        {
+            int notTake = 0 + dp[index - 1][cap];
+            int take = INT_MIN;
+            
+            if (weight[index] <= cap)
+                take = value[index] + dp[index - 1][cap - weight[index]];
+                
+            dp[index][cap] = max (take, notTake);
+        }
+    }
+    
+    return dp[n - 1][maxWt];
+}
+
+
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWt) 
+{
+    vector <vector <int>> dp (n, vector <int> (maxWt + 1, 0));
+    
+    for (int index = 0; index < n; index++)
+    {
+        for (int cap = 0; cap <= maxWt; cap++)
+        {
+            if (index == 0)
+            {
+                dp[index][cap] = (weight[index] <= cap) ? value[index] : 0;
+                continue;
+            }
+
+            int notTake = 0 + dp[index - 1][cap];
+            int take = INT_MIN;
+            
+            if (weight[index] <= cap)
+                take = value[index] + dp[index - 1][cap - weight[index]];
+                
+            dp[index][cap] = max (take, notTake);
+        }
+    }
+    
+    return dp[n - 1][maxWt];
+}
+
+
+
+//Solved by Tabulation Method (Little Space Optimized) (using 2 rows)
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWt) 
+{
+    vector <int>dp (maxWt + 1, 0);
+    
+    for (int i = weight[0]; i <= maxWt; i++)
+        dp[i] = value[0];
+    
+    for (int index = 1; index < n; index++)
+    {
+        vector <int> temp (maxWt + 1, 0);
+        
+        for (int cap = 0; cap <= maxWt; cap++)
+        {
+            int notTake = 0 + dp[cap];
+            int take = INT_MIN;
+            
+            if (weight[index] <= cap)
+                take = value[index] + dp[cap - weight[index]];
+                
+            temp[cap] = max (take, notTake);
+        }
+        
+        dp = temp;
+    }
+    
+    return dp[maxWt];
+}
+
+
+
+//Solved by Tabulation Method (Best Space Optimized) (using only 1 row)
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWt) 
+{
+    vector <int>dp (maxWt + 1, 0);
+    
+    for (int i = weight[0]; i <= maxWt; i++)
+        dp[i] = value[0];
+    
+    for (int index = 1; index < n; index++)
+    {        
+        for (int cap = maxWt; cap >= 0; cap--)          // Here we went from right to left because we wanted to get maximum benefit 
+        {                                               // dp[ind][cap] =  max(dp[ind-1][cap], dp[ind-1][cap-wt[ind]])
+            int notTake = 0 + dp[cap];
+            int take = INT_MIN;
+            
+            if (weight[index] <= cap)
+                take = value[index] + dp[cap - weight[index]];
+                
+            dp[cap] = max (take, notTake);
+        } 
+    }
+    
+    return dp[maxWt];
+}
+
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWt) 
+{
+    vector <int> dp (maxWt + 1, 0);
+    
+    for (int index = 0; index < n; index++)
+    {
+        for (int cap = maxWt; cap >= 0; cap--)
+        {
+            if (index == 0)
+            {
+                dp[cap] = (weight[index] <= cap) ? value[index] : 0;
+                continue;
+            }
+
+            int notTake = 0 + dp[cap];
+            int take = INT_MIN;
+            
+            if (weight[index] <= cap)
+                take = value[index] + dp[cap - weight[index]];
+                
+            dp[cap] = max (take, notTake);
+        }
+    }
+    
+    return dp[maxWt];
+}
