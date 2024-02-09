d17ac7b9c97ca8ded822bb7c8fcf53c81ee5c3a4
Govrani Keyur <202101498@daiict.ac.in>
2024-02-09 21:22:14 +0530

    Solved using stack

--- Day 14 Stack and Queue 2/84. Largest Rectangle in Histogram.cpp
+++ Day 14 Stack and Queue 2/84. Largest Rectangle in Histogram.cpp
@@ -56,6 +56,51 @@ public:
 
 
 
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            left[i] = (!st.empty()) ? st.top() + 1 : 0;
+
+            st.emplace(i);
+        }
+
+        while(!st.empty())
+            st.pop();
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            right[i] = (!st.empty()) ? st.top() - 1 : n - 1;
+
+            st.emplace(i);
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int area = (right[i] - left[i] + 1) * heights[i];
+            maxArea = max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+};
+
+
+
 // Solved using stack (in one pass)
 // Time Complexity :- O(n)
 // Space Complexity :- O(n)
